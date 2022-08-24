1081157e917f5aca35369ece378a0dbf59f30b85
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-24 16:46:00 +0200

    added BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button type="button" class="btn btn-warning fw-bold">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+    name: 'BaseButton',
+    props: {
+        buttonText: String,
+    },
+}
+</script>
+
+<style scoped>
+    .btn{
+        border-radius: 0;
+    }
+
+    .btn-warning{
+        background-color: #d2401e;
+        border-color: #d2401e;
+        color: white;
+    }
+</style>
