48a4ed347b2d8e84ed0744c979238e8201637aef
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-25 14:29:12 +0200

    created SpliSection components

---
+++ src/components/SplitSectionFirst.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="mb-2">
+    <div class="">
+
+    </div>
+    <div class="">
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SplitSectionFirst',
+}
+</script>
+
+<style>
+
+</style>
