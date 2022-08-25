e3b56e2a948a7e718e8d762c7c3c84e8e75d3291
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-25 10:56:16 +0200

    added BannerCard component

---
+++ src/components/BannerCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col">
+        <img :src="card.url" :alt="card.id" class="img-fluid">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BannerCard',
+    props: {
+        card: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
