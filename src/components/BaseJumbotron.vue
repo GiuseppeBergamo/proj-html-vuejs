8e986aff3535a9fd037334df15fe9413db41020c
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-25 15:17:04 +0200

    fixed BaseJumbotron alignment

--- src/components/BaseJumbotron.vue
+++ src/components/BaseJumbotron.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="jumbotron" class="p-5 mb-2">
-    <div class="container p-5">
+    <div class="d-flex justify-content-start align-items-center p-2">
         <div class="w-25 p-2">
             <h1 class="fw-bold text-white text-start">GO AHEAD AND BUILD YOUR OWN PIZZA</h1>
             <h1 class="fw-bold text-white text-start">WE WON'T JUDGE!</h1>
